54eeca1eb91b6afb6685fe579070b2e9722eced0
Mona Jannesar <mona.jannesar@gmail.com>
2025-12-01 12:21:45 -0500

    Remove old folders and apply correct rename

--- shorest_paths/__init__.py
+++
@@ -1,4 +0,0 @@
-from .bfs import bfs_shortest_path
-from .dijkstra import dijkstra_shortest_path
-
-__all__ = ['bfs_shortest_path', 'dijkstra_shortest_path']   
--- shorest_paths/graph.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, List, Tuple, Hashable
-
-Node = Hashable
-unweightedGraph = Dict[Node, List[Node]]
-weightedGraph = Dict[Node, List[Tuple[Node, float]]]
